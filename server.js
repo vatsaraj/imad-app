0f86ca91a93751b3d7cfe07a84415d1d22628696
vatsaraj <vatsaraj@gmail.com>
2017-08-06 20:15:12 +0530

    Changed the logo, added two pages to demo url based redirection.

--- server.js
+++ server.js
@@ -13,8 +13,21 @@ app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
-app.get('/ui/madi.png', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
+app.get('/ui/aloha.png', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'aloha.png'));
+});
+
+
+app.get('/article1', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'article1.html'));
+});
+
+app.get('/article2', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'article2.html'));
+});
+
+app.get('/article3', function (req, res) {
+  res.send('There\'s no such article called \"Article 3\".');
 });
 
 
